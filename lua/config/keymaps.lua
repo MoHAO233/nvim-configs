a2e1fca2258f517fb4c1d35c7a0beeeeb3268463
MoHAO233 <mohao.zarya@gmail.com>
2025-09-24 13:21:53 +0800

    feat: add keymaps for competitest

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,9 @@ opts = {
   noremap = true,
 }
 k.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+k.set("n", "<leader>t", ":CompetiTest run<CR>", opts)
 k.set("n", "<C-h>", "<C-w>h", opts)
 k.set("n", "<C-j>", "<C-w>j", opts)
 k.set("n", "<C-k>", "<C-w>k", opts)
 k.set("n", "<C-l>", "<C-w>l", opts)
+
